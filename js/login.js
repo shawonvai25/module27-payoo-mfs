a5936f8b90842204d12bd5fd718641d9ddd4b238
shawonvai25 <abdullahalshawon25@gmail.com>
2024-10-27 08:57:45 +0600

    added simple login hanlder and preventDefault

--- js/login.js
+++ js/login.js
@@ -1,3 +1,16 @@
 // console.log('absolute button clicking file added')
 
+// document.getElementById('id').addEventListener('click',function)
+
+// form submit reloading the page
+// step- 1: set eventHandler
 document.getElementById('button-login')
+        .addEventListener('click',function(event){
+            // step-2: prevent default behavior (reloading browser)
+            event.preventDefault()
+            console.log('login button clicked')
+
+            // step-3: get the phone number
+           const phoneNumber = document.getElementById('phone-number').value;
+           console.log(phoneNumber)
+        })
