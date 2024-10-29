9e24884a3d58f09eba5be8f5d4447b9a51437a0c
shawonvai25 <abdullahalshawon25@gmail.com>
2024-10-29 13:21:50 +0600

    added realistic input fields

--- js/home.js
+++ js/home.js
@@ -4,7 +4,9 @@
 /*
 S-1: add event handler 
 prevent page reload after form submit
-s-2: get money to be added to the account balance
+s-2: get money to be added to the account balance & also get the pin number
+s-3: verify the pin Number
+
 */  
 
 // step-1: add an event handler to the addmoney button inside the form
@@ -15,9 +17,40 @@ document.getElementById('btn-add-money')
           
             // step-2: get money to be added to the account
             const addMoneyInput = document.getElementById('input-add-money').value;
-            console.log(addMoneyInput);
+            console.log( typeof addMoneyInput);
 
             // get the pin number provided
             const inputPinNumber = document.getElementById('input-pin-number').value;
-            console.log(inputPinNumber);
+            console.log(typeof inputPinNumber);
+
+            // step-3: verify pin number
+
+            // wrong way to validate pin Number
+            if(inputPinNumber === '1234'){
+                console.log('adding money to your account');
+
+                // step-4: get the current balance
+            const balance = document.getElementById('account-balance').innerText;
+            console.log(typeof balance);
+
+            // step-5: add addMoneyInput with balance
+            const addMoneyNumber = parseFloat(addMoneyInput);
+            const balanceNumber = parseFloat(balance);
+            const newBalance = addMoneyNumber + balanceNumber;
+            console.log(newBalance);
+
+            // step-6: update the balance in the UI / DOM
+            document.getElementById('account-balance').innerText = newBalance;
+
+            document.getElementById('input-add-money').value = '';
+            document.getElementById('input-pin-number').value = '';
+
+            
+            }
+            
+            
+          
+            else{
+                alert('failed to add money ! please try again')
+            }
         })
